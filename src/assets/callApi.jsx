c8e0e075f948dab709d8dda5e258b962f3f5e6a3
mmmarciii <mesz.marc@gmail.com>
2025-11-17 20:19:09 +0100

    Update api call

--- src/assets/callApi.jsx
+++ src/assets/callApi.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
+const API_KEY = "2bc77f47cb684ec7b0a150156251611";
+const BASE_URL = "https://api.weatherapi.com/v1/forecast.json";
+
 const callApi = (city) => {
 
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
-
-  const targetUrl = `/api/get-weather?city=${city}&days=3&aqi=no&alerts=no`;
 
     useEffect(()=>{
 
@@ -14,7 +15,7 @@ const callApi = (city) => {
 
         const fetchWeatherData = async() => {
         try {
-            const url = targetUrl;
+            const url = `${BASE_URL}?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`;
 
             const response = await fetch(url);
             const data = await response.json();
@@ -30,8 +31,10 @@ const callApi = (city) => {
         }
         };
 
-      fetchWeatherData();
-    },[city]);
+        if (city) {
+            fetchWeatherData();
+        }
+  }, [city]);
 
   return { weatherData, error };
 }
